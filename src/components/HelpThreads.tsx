e412116c8951b21a2a9813748de7c01b63ce681b
swethanandakumarvh <swetha.nandakumarvh@gmail.com>
2025-06-01 13:47:14 +0530

    Updated Events.tsx

--- src/components/HelpThreads.tsx
+++ src/components/HelpThreads.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { ForumPost } from '../types';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { BellIcon, Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { useAuth } from '../contexts/AuthContext';
