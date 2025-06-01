e7d3ef122898ba81293ead48c00b67622bb803ab
swethanandakumarvh <swetha.nandakumarvh@gmail.com>
2025-06-01 13:46:46 +0530

    Remove unnecessary React imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import BusinessListings from './components/BusinessListings';
